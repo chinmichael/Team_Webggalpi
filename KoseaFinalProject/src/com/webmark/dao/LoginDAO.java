857853f8c31e5696d6f673c1c9db2dcc5f0f2b89
Leeminkyu <Leeminkyu@leeminkyu>
2020-09-15 16:43:35 +0900

    로그인 DAO

--- KoseaFinalProject/src/com/webmark/dao/LoginDAO.java
+++ KoseaFinalProject/src/com/webmark/dao/LoginDAO.java
@@ -1,15 +1,58 @@
 package com.webmark.dao;
 
 public class LoginDAO {
+
+	private String userid;
+	private String userpw;
+	private String username;
+	private String usernick;
+	private String email;
+	private String usertype;
+	private String createdate;
 	
-	private LoginDAO() {
-		
+	public String getUserid() {
+		return userid;
 	}
-	
-	private static LoginDAO instance = new LoginDAO();
-	
-	public static LoginDAO getInstance() {
-		return instance;
+	public void setUserid(String userid) {
+		this.userid = userid;
 	}
+	public String getUserpw() {
+		return userpw;
+	}
+	public void setUserpw(String userpw) {
+		this.userpw = userpw;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUsernick() {
+		return usernick;
+	}
+	public void setUsernick(String usernick) {
+		this.usernick = usernick;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsertype() {
+		return usertype;
+	}
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+
+
 
 }
