8b2f22727edbc817d81877de5dc48b42cdd429fb
opwahmake <63996829+opwahmake@users.noreply.github.com>
2020-09-15 17:29:29 +0900

    Add files via upload

--- KoseaFinalProject/src/com/webmark/dao/AccountDAO.java
+++ KoseaFinalProject/src/com/webmark/dao/AccountDAO.java
@@ -1,4 +1,15 @@
-package com.webmark.dao;
+package com.saeyan.dto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+
 
 public class AccountDAO {
 	
@@ -11,5 +22,43 @@ public class AccountDAO {
 	public static AccountDAO getInstance() {
 		return instance;
 	}
+	
+	public Connection getConnection() throws Exception {
+		Connection conn = null;
+		Context initContext  = new InitialContext();
+		Context envContext  = (Context) initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+		conn = ds.getConnection();
+		return conn;
 
+		}
+	
+	public int modifyAccount(AccountVO mVo) {
+		int result = -1;
+		String sql = "update accountwm user_pw= ?,user_name=?,user_nick = ?,e_mail= ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,mVo.getUserpw());
+			pstmt.setString(2,mVo.getUsername());
+			pstmt.setString(3,mVo.getUsernick());
+			pstmt.setString(4,mVo.getEmail());
+			 result = pstmt.executeUpdate();
+			}catch (Exception e) {
+			e.printStackTrace();
+			}finally {
+			try {
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+			}catch (Exception e) {
+			e.printStackTrace();
+			}
+			}
+			return result;
+			}
+		
+
+	
 }
